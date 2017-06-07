794def944fea590507cae556c367b5dd877d7988
Corey <coreybrooks2@gmail.com>
2017-06-06 21:30:25 -0500

    changed test3 to dynamically create results from this.renderResults, OnClick is sending back data

--- app/components/Main.js
+++ app/components/Main.js
@@ -33,6 +33,25 @@ var Main = React.createClass({
   setTerms: function(search, start, end) {
     this.setState({searchTerm: search, startYear: start, endYear: end});
   },
+  saveData: function(data) {
+     console.log("saveData click is working");
+     const newData = data;
+     console.log("data: " + JSON.stringify(data));
+  },
+  renderResults: function() {
+    if (this.state.results === "") {
+      return
+    }
+    else {
+    return this.state.results.map(
+        (data) => (
+          <div key={data.headline.main} >
+             <Results data={data} handleClick={this.saveData}/>
+          </div>
+        )
+    );
+    }
+  },
 
   //component's render method
   render: function() {
@@ -41,8 +60,22 @@ var Main = React.createClass({
             <div className="jumbotron">
               <h2>Corey's NYT React Searcher</h2>
             </div>
+             
               <Search setTerms={this.setTerms} />
-              <Results results={this.state.results} />
+
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="panel panel-default">
+                    <div className="panel-heading text-center">
+                      <h3 className="panel-title">Results</h3>
+                   </div>
+                   <div className="panel panel-body">
+                     {this.renderResults()} 
+                   </div>
+                 </div>
+                </div>
+              </div>
+
               <Saved />
           </div>
       );
--- app/components/children/Results.js
+++ app/components/children/Results.js
@@ -2,54 +2,29 @@ var React = require("react");
 var helpers = require("./../utils/helpers");
 
 
-//create the Results component
-var Results = React.createClass({
 
-  handleSave: function(event) {
-    console.log("handleSave is working");
-    helpers.saveHistory()
-
-  },
-    
-    render: function() {
+const Results = (props) => {
+  const {web_url, headline, multimedia, byline, section_name, pub_date} = props.data;
+  const {handleClick} = props; 
         return (
-          <div className="row">
-            <div className="col-md-12">
-              <div className="panel panel-default">
-                <div className="panel-heading text-center">
-                  <h3 className="panel-title">Results</h3>
-                </div>
-                {/*this panel will hold the results*/}
-                <div className="panel-body" id="well-section"> 
-                  {this.props.results.map(function(data, i) {
-                      return (                        
-                        <div key={i}>
-                          <hr></hr>
-                          {/*display headline as link*/}
-                          <h4><a href={data.web_url}><i className="fa fa-newspaper-o" aria-hidden="true"></i> {data.headline.main}</a><br/></h4>
-                          <img
-                          className="image"
-                          //ternary expression to display the image if one is available or "sorry" message if not
-                          src={data.multimedia.length ? "https://nytimes.com/" + data.multimedia[0].url: "http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png"} 
-                          />
-                          <p>{data.byline ? data.byline.original : ""}</p>                  
-                          <p>Section: {data.section_name}</p>
-                          {/*ternary expression to display date if one is available*/}
-                          <p>Date: {data.pub_date ? data.pub_date.substring(0,10): "N/A"}</p>
-                          <button 
-                          id={data.headline}
-                          className="btn btn-primary saveButton"
-                          onClick={() => this.handleSave}>save
-                          </button>
-                        </div>
-                      );
-                    })}
-                </div>    
-              </div>
-            </div>
-          </div>
+              <div>
+                  <hr />
+                  {/*display headline as link*/}
+                  <h4><a href={web_url}><i className="fa fa-newspaper-o" aria-hidden="true"></i> {headline.main}</a><br/></h4>
+                  <img
+                    className="image"
+                    //ternary expression to display the image if one is available or "sorry" message if not
+                    src={multimedia.length ? "https://nytimes.com/" + multimedia[0].url: "http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png"} 
+                  />
+                  <p>{byline ? byline.original : ""}</p>                  
+                  <p>Section: {section_name}</p>
+                  {/*ternary expression to display date if one is available*/}
+                  <p>Date: {pub_date ? pub_date.substring(0,10): "N/A"}</p>
+                  <button 
+                    className="btn btn-primary saveButton"
+                    onClick={() => handleClick(props.data)}>save
+                  </button>
+             </div>
         );
     }
-});
-
 module.exports = Results;
