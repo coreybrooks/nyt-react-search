a844e3ddaf0ea7ed95200d68b52bec962ec71347
Corey <coreybrooks2@gmail.com>
2017-06-10 00:03:57 -0500

    cleaned up Saved and added README

--- app/components/children/Saved.js
+++ app/components/children/Saved.js
@@ -7,13 +7,6 @@ const Saved = (props) => {
           <div>
             <hr />
               <h4><a href={url}><i className="fa fa-newspaper-o" aria-hidden="true"></i> {title}</a><br/></h4>
-              {/*<img
-                className="image"
-                //ternary expression to display the image if one is available or "sorry" message if not
-                src={multimedia.length ? "https://nytimes.com/" + multimedia[0].url: "http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png"} 
-              />
-              <p>{byline ? byline.original : ""}</p>
-              <p>Section: {section_name}</p>*/}
               {/*ternary expression to display date if one is available*/}
               <p>Date: {date ? date.substring(0,10): "N/A"}</p>
               <button 
